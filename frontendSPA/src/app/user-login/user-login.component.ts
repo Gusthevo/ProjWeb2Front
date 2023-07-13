8b7563973d95ac91d9188ef280791d02f05fb0f3
Gustavo Campelo <gustavocraft321@gmail.com>
2023-07-12 23:00:36 -0300

    commit frontend

---
+++ frontendSPA/src/app/user-login/user-login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../user';
+import { LoginuserService } from '../loginuser.service';
+
+@Component({
+  selector: 'app-user-login',
+  templateUrl: './user-login.component.html',
+  styleUrls: ['./user-login.component.css']
+})
+export class UserLoginComponent implements OnInit {
+
+  user: User = new User();
+
+  constructor(private loginuserService: LoginuserService) {}
+
+  ngOnInit(): void {}
+
+  userLogin() {
+    console.log(this.user); // Exibe no console o usuário e a senha inseridos
+    this.loginuserService.loginUser(this.user).subscribe(
+      data => {
+        alert("Login realizado com sucesso!");
+      },
+      error => {
+        alert("Login falhou, insira corretamente o Usuário e senha");
+      }
+    );
+  }
+}
